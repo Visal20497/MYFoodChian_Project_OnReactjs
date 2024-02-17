5913cf98a80218ea99ff176e461b6a5e2465cfd0
Vishal Maheshwari <147162171+Visal20497@users.noreply.github.com>
2024-02-17 18:43:46 +0530

    Update HowItWorks.jsx

--- src/components/AddRestaurantComponents/HowItWorks/HowItWorks.jsx
+++ src/components/AddRestaurantComponents/HowItWorks/HowItWorks.jsx
@@ -18,9 +18,9 @@ let HowItWorks = () => {
             <div className={css.div2}>
                 <div className={css.title}>How it works?</div>
                 <div className={css.cards}>
-                    <Card5 step='1' title='Create your page on Zomato' tag='Help users discover your place by creating a listing on Zomato' img={bookIcon} color='#FFFCF1' />
-                    <Card5 step='2' title='Register for online ordering' tag='And deliver orders to millions of customers with ease' img={bikeIcon} color='#FFF5F6' />
-                    <Card5 step='3' title='Start receiving orders online' tag='Manage orders on our partner app, web dashboard or API partners' img={packageIcon} color='#FFF9F1' />
+                    <Card5 step='1' title='Create-MyFoodChain' tag='Help users discover your place by creating a listing on MyFoodChain' img={bookIcon} color='#FFFCF1' />
+                    <Card5 step='2' title='Register-MyFoodChain' tag='And deliver orders to millions of customers with ease' img={bikeIcon} color='#FFF5F6' />
+                    <Card5 step='3' title='orders-MyFoodChain' tag='Manage orders on our partner app, web dashboard or API partners' img={packageIcon} color='#FFF9F1' />
                 </div>
             </div>
         </div>
