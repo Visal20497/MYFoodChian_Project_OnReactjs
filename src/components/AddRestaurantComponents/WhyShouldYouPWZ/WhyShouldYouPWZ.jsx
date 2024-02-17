1ae6715568ad0ee22ec2ea866ad147ea01a4a348
Vishal Maheshwari <147162171+Visal20497@users.noreply.github.com>
2024-02-17 18:44:53 +0530

    Update WhyShouldYouPWZ.jsx

--- src/components/AddRestaurantComponents/WhyShouldYouPWZ/WhyShouldYouPWZ.jsx
+++ src/components/AddRestaurantComponents/WhyShouldYouPWZ/WhyShouldYouPWZ.jsx
@@ -9,8 +9,8 @@ import css from './WhyShouldYouPWZ.module.css';
 let WhyShouldYouPWZ = () => {
     return <div className={css.outerDiv}>
         <div className={css.innerDiv}>
-            <div className={css.title}>Why should you partner with Tomato?</div>
-            <div className={css.tagLine}>Tomato enables you to get 60% more revenue, 10x new customers and boost your brand visibility by providing insights to improve your business.</div>
+            <div className={css.title}>Why should you partner with MyFoodChain?</div>
+            <div className={css.tagLine}>MyApp enables you to get 60% more revenue, 10x new customers and boost your brand visibility by providing insights to improve your business.</div>
             <div className={css.cards}>
                 <Card4 imgSrc={globIcon} title="500+ cities" tagLine="in India" />
                 <Card4 imgSrc={calenderIcon} title="2.5 lakhs+" tagLine="restaurant listings" />
